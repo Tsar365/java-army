5e0c85820bb05be38d1966759a4c5a16f8590bea
Ehasanul Haque <ehasanmd365@gmail.com>
2026-06-14 08:25:05 +0600

    abstraction updated

---
+++ src/main/java/lecture17/abstractor.java
@@ -0,0 +1,47 @@
+package main.java.lecture17;
+
+public class abstractor {
+
+    public static void main(String[] args) {
+        Car car = new FuelCar();
+        car.start();
+        car.accelerate();
+        car.brake();
+    }
+}
+
+ abstract class Car { //can not create object of abstract class
+     void start() {
+         System.out.println("Car started");
+     }
+
+     abstract void accelerate();
+
+     abstract void brake();
+ }
+
+ class FuelCar extends Car {  //abstarct er sobgula method implement kra lagbe
+
+     @Override
+     void accelerate() {
+         System.out.println("Fuel car is accelerating");
+     }
+
+     @Override
+     void brake() {
+         System.out.println("Fuel car is stopping");
+     }
+
+ }
+
+ class ElectricCar extends Car {
+     @Override
+     void accelerate() {
+         System.out.println("Electric car is accelerating");
+     }
+
+     @Override
+     void brake() {
+         System.out.println("Electric car is stopping");
+     }
+}
